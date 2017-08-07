3ffdf2fbd055d723b8ad0d67a2a8ed0e74d122d0
Nickkal17 <30806371+Nickkal17@users.noreply.github.com>
2017-08-07 22:39:14 +0300

    Test commit

--- app/src/main/java/com/example/test/myapplication/MainActivity.java
+++ app/src/main/java/com/example/test/myapplication/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //hello world
+        //this is for brunch
     }
 }
